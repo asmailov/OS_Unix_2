ac0665e534866aad86e56e6763e40ccd525eb41c
Aleksandr <asmailov.reg@gmail.com>
2014-10-13 19:55:57 +0300

    Added printWelcome(), printPromt(), modified init().

--- pipe_func.c
+++ pipe_func.c
@@ -1,9 +1,28 @@
 #include "pipe_func.h"
 
 void init()
-{
+{	
+	// Getting username, hostname and currDir
 	userName = getenv("USER");
 	gethostname(hostName, HOST_NAME_MAX);
+
+	printWelcome();
+	printPromt();
+}
+
+void printWelcome()
+{
+	puts("");
+    puts("Jus sveikina:");
+	puts("IV kurso, I grupės");
+	puts("Informatikos (matematinės info) studentas,");
+	puts("Aleksandr Šmailov");
+    puts("Sėkmės naudojantis mano shell'u.");
+    puts("");
+}
+
+void printPromt()
+{
 	getcwd(currDir, PATH_MAX);
 	printf("%s@%s:~%s$ ", userName, hostName, currDir);
 }
--- pipe_func.h
+++ pipe_func.h
@@ -6,11 +6,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
+#include <time.h>
 
 char *userName;
 char hostName[HOST_NAME_MAX];
 char currDir[PATH_MAX];
 
 void init();
+void printWelcome();
+void printPromt();
 
 #endif
